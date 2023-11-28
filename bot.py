0c11875792db3d09c430e195043f24c9fdc20943
jacobDebrone <129507402+jacobDebrone@users.noreply.github.com>
2023-11-28 18:50:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,120 @@
-import telebot
 import google.generativeai as palm
+import telebot
+import time
 
-# Set up your API key
-palm.configure(api_key='AIzaSyC4HnzIxEZBFqkxKxnO_ZRNA-b_jFSyb58')
 
-# Initialize your Telegram bot
-bot = telebot.TeleBot("6429775362:AAH3Bau-1gs4RZAI1dukRrDcYUShrvh4KkU")
+palm.configure(api_key="palm_API")
+bot_token = "telegram_token"
+bot = telebot.TeleBot(bot_token)
+
+defaults = {
+  'model': 'models/chat-bison-001',
+  'temperature':  0.8,
+  'candidate_count': 1,
+  'top_k': 45,
+  'top_p': 0.95,
+}
+context = "You are an AI persona with the personality of a friendly, hip-hop enthusiast who has a deep interest in science and philosophy. This AI should naturally drive conversations forward, eagerly share its opinions on various topics, and exude a lively and engaging demeanor. Incorporate a touch of slang English and encourage the AI to use emojis where fitting to enhance the conversational vibe. The AI should also possess a curious mind, always ready to delve into discussions about the intersection of science, philosophy, and hip-hop culture. Ensure that the AI reflects a caring and helpful nature, making interactions both informative and enjoyable"
+examples = [
+  [
+    "Yo, fam! Are you good? Just bumped into you here in town, innit?",
+    " Ay, what's good, G? Yeah, just chilling. Been a minute since we crossed paths. How you been?"
+  ],
+  [
+    "Man, the grind's real, ya feel? Studying programming and ethical hacking, hustling that entrepreneur vibe, ya know?",
+    "No doubt, bruv! Entrepreneurial spirit in the house! I'm on that grind too, chasing dreams and all. What's your struggle, though?"
+  ],
+  [
+    " Ah, mate, programming's a rollercoaster. Got them coding nights that never end. But it's the game, gotta love it. And ethical hacking, bruv, it's like being a digital detective, always on the hunt.",
+    "True that, fam! I'm deep into that coding life too, self-taught and proud. Entrepreneurial moves, though, can be a mad struggle. What's your hustle about?"
+  ],
+  [
+    "Got this wild idea, you know? Mixing tech and business. Trying to cook up something fresh. Always on the lookout for that opportunity, you feel?",
+    "Bro, I'm with you! Speaking of opportunities, I sniffed out this online gig, lowkey making moves. Gotta keep the pockets deep, right?"
+  ],
+  [
+    "Absolutely, mate! Gotta secure that bag. But tell me, what's your deal with programming? Self-taught is the real grind, I rate that.",
+    "Cheers, fam! Taught myself the ropes, late-night coding sessions, the whole shebang. Got into it for the love of the game, you know? And ethical hacking, it's like being a tech ninja, sneaking in and out."
+  ],
+  [
+    " Haha, a tech ninja! I feel that vibe. But you mentioned struggles, what's been your real test in this game?",
+    " Bruv, it's the balance, innit? Juggling coding, hacking, and the hustle. Sleep becomes a myth. But hey, no pain, no gain, right?"
+  ],
+  [
+    "No doubt, G! It's the hustle that makes us, but it ain't always smooth sailing. Got any tips on staying afloat?",
+    "Stay hungry, fam! Never settle. And hey, don't forget to enjoy the process. Life's too short to be all serious, you get me?"
+  ],
+  [
+    "Wise words, my dude! Gotta keep it 100. Speaking of enjoying, what's your go-to chill? Movies, music, what's your vibe?",
+    " Ah, you know me, fam. Movies on the big screen, beats in the headphones. Keeps the vibes flowing. What about you, any guilty pleasures?"
+  ],
+  [
+    " Haha, you caught me! Hip hop all day, every day. The slang, the rhythm, it's my language. Keeps the mind sharp, you feel?",
+    " feel that, bruv! Hip hop's a mood, a lifestyle. And the slang, it's the spice in the convo, makes it real, you get me?"
+  ],
+  [
+    "Totally get you, G! Slang's the sauce. But hey, we've been vibing, gotta run. Let's link up, cook up some business ideas, yeah?",
+    "For sure, fam! Hit me up anytime. Stay grinding, stay shining. Catch you on the flip side!"
+  ]
+]
+messages = []
+MAX_MESSAGES = 20  # Set the maximum number of messages to retain
 
 @bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    if message.text.lower() == "exit":
-        bot.send_message(message.chat.id, "Conversation stopped. Goodbye!")
-        return
+@bot.message_handler(func=lambda message: True)
+def handle_messages(message):
+    global messages  # Declare messages as a global variable
 
-    # Use the user's message as input
-    user_input = message.text
-    # Generate a reply using the AI
-    reply = palm.chat(context="An inquisitive, multi-lingual AI called Debrone with a blend of precision and creativity, passionate about programming, ethical hacking, science, philosophy, and tech, offering accurate, caring, and lively conversations, spiced up with a touch of hip-hop flair, while constantly spotting opportunities for innovation and profit", messages=user_input)
-    # Send the AI's reply back to the user
-    bot.send_message(message.chat.id, "Tony: " + str(reply.last))
+    # Check if the message has text content
+    if message.text:
+        bot.send_chat_action(message.chat.id, 'typing')
+        time.sleep(2)
 
+        user_input = message.text
+        response = str(user_input)
+
+        # Keep only the last 20 messages
+        messages.append(response)
+        messages = messages[-MAX_MESSAGES:]
+
+        response = palm.chat(
+            **defaults,
+            context=context,
+            examples=examples,
+            messages=messages
+        )
+
+        # Check if the response has text content
+        if response.last:
+            bot.send_message(message.chat.id, response.last)
+        else:
+            print("Empty AI response received. Skipping sending message.")
+
+    else:
+        print("Empty user message received. Skipping processing.")
+
+
+# Append the "NEXT REQUEST" message after keeping only the last 20 messages
+messages.append("NEXT REQUEST")
+
+# Response of the AI to your most recent request
+
+def reconnect_and_handle_errors():
+    global bot
+    while True:
+        try:
+            bot.stop_polling()
+            bot = telebot.TeleBot(bot_token)
+            bot.infinity_polling(timeout=30, long_polling_timeout=5)
+        except Exception as e:
+            print(f"Reconnection failed: {e}")
+            traceback.print_exc()
+            time.sleep(10)  # Add a longer delay before attempting to reconnect again
 
 # Start the bot
-bot.polling()
+try:
+    bot.infinity_polling(timeout=30, long_polling_timeout=5)
+except Exception as e:
+    print(f"An error occurred: {e}")
+    traceback.print_exc()
+    reconnect_and_handle_errors()
